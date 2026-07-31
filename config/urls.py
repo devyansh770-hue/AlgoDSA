83051d22e4040e5a86c19bd72f5484bc2ffb2c01
Devyansh Verma <devyansh770@gmail.com>
2026-07-31 10:36:21 +0530

    Add root URL routes for /simulator/, /mistakes/, /interview/, and /career/

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 from apps.users.views import landing_page
 
 from apps.problems import views as problem_views
+from apps.submissions import views as submission_views
+from apps.progress import views as progress_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +18,10 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('learn/', problem_views.learn_hub_view, name='learn_hub'),
     path('learn/<slug:topic_slug>/', problem_views.learn_topic_view, name='learn_topic'),
+    path('simulator/', problem_views.algorithm_simulator, name='algorithm_simulator'),
+    path('interview/', problem_views.mock_interview, name='mock_interview'),
+    path('mistakes/', submission_views.mistake_library, name='mistake_library'),
+    path('career/', progress_views.career_readiness, name='career_readiness'),
     path('topics/', include('apps.problems.urls')),
     path('submissions/', include('apps.submissions.urls')),
     path('dashboard/', include('apps.progress.urls')),
