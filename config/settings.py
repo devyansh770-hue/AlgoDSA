cd971803eb21dc8ae7d4c0bce0a28ac7c246e840
Devyansh Verma <devyansh770@gmail.com>
2026-08-02 18:06:45 +0530

    fix(static): set absolute STATIC_URL with leading slash and enable static serving in urls.py

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
@@ -127,8 +127,9 @@ STORAGES = {
 
 
 # Media files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
